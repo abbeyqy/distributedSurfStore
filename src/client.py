7a4153b7dd429a9cfe469fc19529eb525b80ee0f
Yudong Diao <ydiao@umass.edu>
2019-11-10 14:09:32 -0800

    create index.txt if not exists

--- src/client.py
+++ src/client.py
@@ -16,25 +16,35 @@ if __name__ == "__main__":
         # Test ping
         client.surfstore.ping()
         print("Ping() successful")
+
         ##### sync file #####
+        localFile = {}
         localNewFile = {}
         localUpdatedFile = {}
+
         # open local index file
-        localFileInfo = {
-        }  # key : value = 'xxx.jpg' : [2 ['e52a', '928f', '11c3']]
+        localFileInfo = {}
+
+        # if index.txt does not exist, create
+        if not os.path.exists(args.basedir + "index.txt"):
+            with open(args.basedir + "index.txt", 'w'): pass
+
+        # key : value = 'xxx.jpg' : [2 ['e52a', '928f', '11c3']]
         with open(args.basedir + "index.txt") as f:
             for line in f:
                 linelist = line.split()
                 localFileInfo[linelist[0]] = [linelist[1]].append(linelist[2:])
+
         # client scan base directory
         for filename in os.listdir(args.basedir):
+            print(filename)
             if filename == "index.txt":
                 continue
             hashlist = []
             with open(args.basedir + filename, "rb") as bytefile:
                 while True:
                     piece = bytefile.read(args.blocksize)
-                    if piece == "":
+                    if piece == b'':
                         break
                     h = hashlib.sha256(piece).hexdigest()
                     hashlist.append(h)
